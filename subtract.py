a4529fd78a8251898c9a194d55236cb3bedad49c
sabrinaqi <sabrina.qi@duke.edu>
2020-02-05 12:56:47 -0500

    made subtract function

---
+++ subtract.py
@@ -0,0 +1,15 @@
+def subtract(a,b):
+    answer = a-b
+    return answer
+
+
+def interface():
+    a = input("Type a number: ")
+    b = input("Type another number: ")
+    a = float(a)
+    b = float(b)
+    answer = subtract(a,b)
+    print("The difference between {} and {} is {}".format(a, b, answer))
+
+if __name__ == "__main__":
+		interface()
